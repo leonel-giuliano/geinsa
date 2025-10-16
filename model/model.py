fe9fd3440e569f3b414f55cedbd4bbf97de5cc4c
leonel-giuliano <leonelgiuliano7@gmail.com>
2025-10-15 21:52:38 -0300

    Load pkl and fix function arguments

--- model/model.py
+++ model/model.py
@@ -1,3 +1,16 @@
+import re
+import joblib
+import pandas as pd
+
+from sentence_transformers import util
+
+
+UID_PRED = 0
+
+SCORE_IX = 1
+MODEL_SCORE_IX = 2
+
+
 def strnormalize(s):
     rep_accent = str.maketrans("áéíóúü", "aeiouu")
 
@@ -16,7 +29,7 @@ def tokenize(df, cols, join=False) -> pd.Series:
     return tk if not join else tk.apply(" ".join)
 
 
-def semantic_rank(query, top_k=50):
+def semantic_rank(query, embedder, article_embeddings, top_k=50):
     # Get the embeddings of the query used in order to get similar words
     query_emb = embedder.encode(query, convert_to_tensor=True)
 
@@ -30,13 +43,10 @@ def semantic_rank(query, top_k=50):
     return ranked
 
 
-def recommend(query, top_k=50):
+def recommend(query, model, embedder, article_embeddings, aid_map, item_features, top_k=50):
     # Get the articles thar are going to be ranked
-    candidates = semantic_rank(query, top_k)
+    candidates = semantic_rank(query, embedder, article_embeddings, top_k)
     article_ids = [aid for aid, _ in candidates]
-
-    # Recommend with the LightFM model
-    _, _, aid_map, _ = ds.mapping()
 
     aids = [aid_map[aid] for aid in article_ids]
     scores = model.predict(UID_PRED, aids, item_features=item_features)
@@ -51,6 +61,14 @@ def recommend(query, top_k=50):
 
 
 def main():
+    data = joblib.load("model-data.pkl")
+
+    model = data["model"]
+    item_features = data["item_features"]
+    ds = data["dataset"]
+    embedder = data["embedder"]
+    article_embeddings = data["article_embeddings"]
+
     articles = pd.read_csv("/content/drive/MyDrive/Geinsa/data/noticias_econojournal_completo.csv")
     articles = articles.rename(columns={
         "ID": "id",
@@ -59,9 +77,12 @@ def main():
         "Cuerpo": "body"
     })
 
-    results = recommend("dólar", 3)
+    _, _, aid_map, _ = ds.mapping()
+
+    results = recommend("dólar", model, embedder, article_embeddings, aid_map, item_features, 3)
     for aid, _, _ in results:
-        print(f"{aid} | {articles.loc[articles["id"] == aid]["title"].squeeze()}")
+        temp = articles.loc[articles["id"] == aid]["title"].squeeze()
+        print(f"{aid} | {temp}")
 
 
 if __name__ == "__main__":
